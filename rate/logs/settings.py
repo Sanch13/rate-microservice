f40c6313e78103acca274eac6457b43943334f37
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2023-04-13 22:40:42 +0300

    Delete rate/logs directory
    del log

--- rate/logs/settings.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-
-logger_1 = logger.bind()
-logger_1.add("./logs/logs.log",
-             rotation="10 MB",
-             format="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | {function}:{line} | {message}",
-             compression="zip")
